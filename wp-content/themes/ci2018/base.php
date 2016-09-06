3d83f7a921c5dabebab8a7179f24a3db491aca7e
developer <developer@developers-MacBook-Pro.local>
2016-09-06 14:09:21 -0400

    Base styling and setup

--- wp-content/themes/ci2018/base.php
+++ wp-content/themes/ci2018/base.php
@@ -15,19 +15,20 @@ use Roots\Sage\Wrapper;
       </div>
     <![endif]-->
     <?php
-      do_action('get_header');
-      get_template_part('templates/header');
+      // do_action('get_header');
+      // get_template_part('templates/header');
     ?>
     <div class="wrap container" role="document">
       <div class="content row">
-        <main class="main">
-          <?php include Wrapper\template_path(); ?>
-        </main><!-- /.main -->
-        <?php if (Setup\display_sidebar()) : ?>
+        <!-- <?php if (Setup\display_sidebar()) : ?> -->
           <aside class="sidebar">
             <?php include Wrapper\sidebar_path(); ?>
           </aside><!-- /.sidebar -->
-        <?php endif; ?>
+        <!-- <?php endif; ?> -->
+        <main class="main">
+          <?php include Wrapper\template_path(); ?>
+        </main><!-- /.main -->
+
       </div><!-- /.content -->
     </div><!-- /.wrap -->
     <?php
---
+++ wp-content/themes/ci2018/template-home.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Home
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <!-- <?php get_template_part('templates/page', 'header'); ?> -->
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
+
+<?php
+  if (has_nav_menu('primary_navigation')) :
+    wp_nav_menu(['theme_location' => 'primary_navigation', 'container' => '', 'menu_class' => '', 'items_wrap' => '<ol id="%!$s" class="%2$s">%3$s</ol>']);
+  endif;
+?>
+
+<?php get_search_form(); ?>
