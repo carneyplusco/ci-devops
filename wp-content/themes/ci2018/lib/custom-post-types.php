0dc142372ba932f93d963d3135f6885eb7f5e006
developer <developer@developers-MacBook-Pro.local>
2016-09-07 11:15:29 -0400

    Change post type to Readings

--- wp-content/themes/ci2018/lib/custom-post-types.php
+++ wp-content/themes/ci2018/lib/custom-post-types.php
@@ -1,20 +1,20 @@
 <?php
-register_post_type('publication',
+register_post_type('reading',
     array(
       'labels' => array(
-        'name' => __('Publications'),
-        'singular_name' => __('Publication'),
+        'name' => __('Readings'),
+        'singular_name' => __('Reading'),
         'add_new' => __('Add New' ),
-        'add_new_item' => __('Add New Publication'),
+        'add_new_item' => __('Add New Reading'),
         'edit' => __('Edit'),
-        'edit_item' => __('Edit Publication'),
-        'new_item' => __('New Publication'),
-        'view' => __('View Publication'),
-        'view_item' => __('View Publication'),
-        'search_items' => __('Search Publications'),
-        'all_items' => __('All Publications'),
-        'not_found' => __('No publications found.'),
-        'not_found_in_trash' => __('No publications found in Trash')
+        'edit_item' => __('Edit Reading'),
+        'new_item' => __('New Reading'),
+        'view' => __('View Reading'),
+        'view_item' => __('View Reading'),
+        'search_items' => __('Search Readings'),
+        'all_items' => __('All Readings'),
+        'not_found' => __('No readings found.'),
+        'not_found_in_trash' => __('No readings found in Trash')
       ),
       'public' => true,
       'hierarchical' => false,
@@ -22,7 +22,7 @@ register_post_type('publication',
       'supports' => array(
         'title', 'editor', 'thumbnail', 'excerpt'
       ),
-      'rewrite' => array('slug' => 'publications', 'with_front' => false),
+      'rewrite' => array('slug' => 'readings', 'with_front' => false),
       'menu_icon' => 'dashicons-awards'
     )
   );
