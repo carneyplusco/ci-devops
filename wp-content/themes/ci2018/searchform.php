4665f3a5ba7d0548ad6ee46ffdcf6d288aec9fc6
developer <developer@ryan.local>
2016-09-16 12:27:42 -0400

    Removed unused php

--- wp-content/themes/ci2018/searchform.php
+++ wp-content/themes/ci2018/searchform.php
@@ -1,11 +1,9 @@
-<!--form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>"-->
 <form class="search-form">
     <label>
         <input type="search"
             placeholder="<?php echo esc_attr_x( 'SEARCH', 'placeholder' ) ?>"
             value="<?php echo get_search_query() ?>" name="s"
              />
+        <span class="input-underline"></span>
     </label>
 </form>
-
-<!-- <input class="search-form" placeholder="SEARCH"></input> -->
