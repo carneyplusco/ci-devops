69fd76424a78038ca529f54c7e55e1832e0f50d8
developer <developer@ryan.local>
2016-09-16 11:46:10 -0400

    Add ajax loading of programs and relevant layout changes

--- wp-content/themes/ci2018/templates/header.php
+++ wp-content/themes/ci2018/templates/header.php
@@ -1,12 +1,17 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+<?php $title = get_the_title(); ?>
+<?php $menu_items = wp_get_nav_menu_items('Home Nav'); ?>
+<?php
+  $title_number = 0;
+  foreach ($menu_items as $key => $value) {
+    if (strtoupper($value->title) == strtoupper($title)) {
+      $title_number = $key + 1;
+    }
+  }
+?>
+
+<header>
+  <?php
+    echo '<a id="page-title" href="/' . strtolower($title) . '"><span class="section-number">' . $title_number . '.</span>' . $title . '</a>';
+  ?>
+  <a class="back-button" href="/">BACK</a>
 </header>
