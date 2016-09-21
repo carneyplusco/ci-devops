58ed9d8363ae049d3087276adabbf2813afe8d38
developer <developer@ryan.local>
2016-09-21 13:38:56 -0400

    Fix relative svg urls

--- wp-content/themes/ci2018/assets/scripts/home-bg.js
+++ wp-content/themes/ci2018/assets/scripts/home-bg.js
@@ -5,8 +5,15 @@
       var characters = ["c", "i", "5", "7", "1", "8"],
           i          = 1;
 
+      $('.content').addClass("icon-c");
+
       setInterval(function(){
-        $('.main').css('background', "url('wp-content/themes/ci2018/assets/images/CI5718/ci-" + characters[i] + ".svg') no-repeat top");
+        if (i != 0) {
+          $('.content').removeClass("icon-" + characters[i-1]);
+        } else {
+          $('.content').removeClass("icon-" + characters[5]);
+        }
+        $('.content').addClass("icon-" + characters[i]);
 
         i = (i + 1) % 6;
       }, 10000);
