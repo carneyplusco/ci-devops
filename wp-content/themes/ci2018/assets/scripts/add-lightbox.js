27f30dd3addbf557725f1438c0415cc93d13aecf
developer <developer@ryan.local>
2016-09-23 16:01:55 -0400

    Remove console log from script

--- wp-content/themes/ci2018/assets/scripts/add-lightbox.js
+++ wp-content/themes/ci2018/assets/scripts/add-lightbox.js
@@ -1,13 +1,13 @@
 (function($) {
 
   $(document).ready(function() {
-    console.log($('img'));
 
     $('img').on('click', function() {
       if(!$(this).is('.featherlight-inner')) {
         $.featherlight($(this));
       }
     });
+    
   });
 
 })(jQuery);
