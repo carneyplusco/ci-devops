8e754f6b1639b075c77e1734fb5168b1ed294e17
Jesse Hixson <hixsonj@gmail.com>
2017-01-17 11:49:27 -0500

    Updates 404 template. Closes #73

--- wp-content/themes/ci2018/404.php
+++ wp-content/themes/ci2018/404.php
@@ -1,7 +1,17 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php use Roots\Sage\Extras; ?>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
-</div>
+<section class="page-header">
+  <h1 class="page-header__title"><span class="page-title">Page not found</span></h1>
+  <a class="page-header__back-button" href="<?= Extras\back_link() ?>" aria-label="Go back to previous page" tabindex="0">Back</a>
+</section>
 
-<?php get_search_form(); ?>
+<section class="page-content">
+  <div class="page-content__body">
+    <p>Sorry, we couldn't find the page you were looking for.</p>
+  </div>
+
+  <section class="page-header">
+    <span class="section-number"><i class="icon-search"></i></span>
+    <?php get_search_form(); ?>
+  </section>
+</section>
