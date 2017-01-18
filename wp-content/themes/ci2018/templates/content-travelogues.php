2659ddf30fd45d9f69dfe1893f61ffe9a3ab6af5
Jesse Hixson <hixsonj@gmail.com>
2017-01-18 16:44:17 -0500

    Updates travelogues blog query. Adds condition for no returned posts. Closes #82

--- wp-content/themes/ci2018/templates/content-travelogues.php
+++ wp-content/themes/ci2018/templates/content-travelogues.php
@@ -4,18 +4,24 @@
   $menu_items = wp_get_nav_menu_items('Home Nav');
   $menu_number = Extras\menu_number('Home Nav', get_the_title());
 
-  $ci_blog_category = 25;
-  $posts = json_decode(file_get_contents("http://blog.cmoa.org/wp-json/wp/v2/posts?categories={$ci_blog_category}"));
+  $travelogue_tag_id = 113;
+  $posts = json_decode(file_get_contents("http://blog.cmoa.org/wp-json/wp/v2/posts?tags={$travelogue_tag_id}&order=asc"));
   $post_count = count($posts);
 ?>
 
 <div class="article-list">
-  <?php foreach($posts as $p): ?>
-    <span class="article-list__section-number"><?= $menu_number .'.'. $post_count-- ?></span>
-    <article class="article-item" vocab="http://schema.org/" typeof="NewsArticle">
-      <h2 property="headline"><a property="url" href="<?= $p->link ?>" class="external-link"><?= $p->title->rendered ?></a></h2>
-      <span class="screen-reader-text">(Link opens on an external site)</span>
-      <?= wpautop($p->post_list_text) ?>
-    </article>
-  <?php endforeach; ?>
+  <?php if($post_count): ?>
+    <?php foreach($posts as $p): ?>
+      <span class="article-list__section-number"><?= $menu_number .'.'. $post_count-- ?></span>
+      <article class="article-item" vocab="http://schema.org/" typeof="NewsArticle">
+        <h2 property="headline"><a property="url" href="<?= $p->link ?>" class="external-link"><?= $p->title->rendered ?></a></h2>
+        <span class="screen-reader-text">(Link opens on an external site)</span>
+        <?= wpautop($p->post_list_text) ?>
+      </article>
+    <?php endforeach; ?>
+  <?php else: ?>
+    <div class="article-item">
+      <p>No posts were found. Check back soon.</p>
+    </div>
+  <?php endif; ?>
 </div>
