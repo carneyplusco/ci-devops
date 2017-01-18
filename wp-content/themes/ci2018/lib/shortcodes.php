0d5c3049aaf6d4c7bbf6a089f6da54ea2fec24fc
Jesse Hixson <hixsonj@gmail.com>
2017-01-18 11:39:31 -0500

    Adds embed styles for responsive media and shortcodes for youtube and vimeo

--- wp-content/themes/ci2018/lib/shortcodes.php
+++ wp-content/themes/ci2018/lib/shortcodes.php
@@ -6,7 +6,7 @@ use Roots\Sage\Assets;
  * Returns app store badge svg with link to app
  */
 function app_store_badge() {
-  return '<a href="https://itunes.apple.com/us/app/carnegie-museum-of-art/id700992890?mt=8" title="Download our app on the App Store">'.Assets\inline_svg('images/app-store-badge.svg').'<span class="screen-reader-text">Download our app on the App Store</span></a>';
+  return '<a href="https://itunes.apple.com/us/app/carnegie-museum-of-art/id700992890?mt=8" title="Download our app on the App Store">'.Assets\inline_svg('images/app-store-badge.svg').'<span class="screen-reader-text">Download on the App Store</span></a>';
 }
 
 /**
@@ -17,11 +17,41 @@ function app_store_link() {
 }
 
 /**
+ * Returns Vimeo embed
+ * [vimeo id=""]
+ */
+function vimeo($atts) {
+	$a = shortcode_atts(
+		array(
+			'id' => '',
+		),
+		$atts
+	);
+	return '<div class="embed"><iframe src="https://player.vimeo.com/video/'.$a['id'].'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>';
+}
+
+/**
+ * Returns Youtube embed
+ * [youtube id=""]
+ */
+function youtube($atts) {
+	$a = shortcode_atts(
+		array(
+      'id' => '',
+    ),
+    $atts
+  );
+  return '<div class="embed"><iframe src="http://www.youtube.com/embed/'.$a['id'].'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>';
+}
+
+/**
  * Register shortcodes
  */
 function register_shortcodes() {
 	add_shortcode('app_store_badge', __NAMESPACE__ . '\\app_store_badge');
   add_shortcode('app_store_link', __NAMESPACE__ . '\\app_store_link');
+  add_shortcode('vimeo', __NAMESPACE__ . '\\vimeo');
+  add_shortcode('youtube', __NAMESPACE__ . '\\youtube');
 }
 add_action('init', __NAMESPACE__ . '\\register_shortcodes');
 
